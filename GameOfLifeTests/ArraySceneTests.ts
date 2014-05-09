84f1ac46af6ce2ad82d21da109ab692e95d663cf
cfuehrmann <cfuhrmann@gmail.com>
2014-05-09 16:03:46 +0200

    Fixed the placement of some brackets

--- GameOfLifeTests/ArraySceneTests.ts
+++ GameOfLifeTests/ArraySceneTests.ts
@@ -17,61 +17,71 @@ test("Get", () => {
 test("WidthNonInteger", () =>
     throws(() => new ArrayScene(1.5, 7),
         (e: ArgumentException) => e.getArgumentName() === "width",
-        "An ArgumentException with argument name 'width' is thrown")
+        "An ArgumentException with argument name 'width' is thrown"
+        )
     );
 
 test("WidthNaN", () =>
     throws(() => new ArrayScene(Number.NaN, 7),
         (e: ArgumentException) => e.getArgumentName() === "width",
-        "An ArgumentException with argument name 'width' is thrown")
+        "An ArgumentException with argument name 'width' is thrown"
+        )
     );
 
 test("WidthNegativeInfinity", () =>
     throws(() => new ArrayScene(Number.NEGATIVE_INFINITY, 7),
         (e: ArgumentException) => e.getArgumentName() === "width",
-        "An ArgumentException with argument name 'width' is thrown")
+        "An ArgumentException with argument name 'width' is thrown"
+        )
     );
 
 test("WidthPositiveInfinity", () =>
     throws(() => new ArrayScene(Number.POSITIVE_INFINITY, 7),
         (e: ArgumentException) => e.getArgumentName() === "width",
-        "An ArgumentException with argument name 'width' is thrown")
+        "An ArgumentException with argument name 'width' is thrown"
+        )
     );
 
 test("WidthNonPositive", () =>
     throws(() => new ArrayScene(0, 7),
         (e: ArgumentException) => e.getArgumentName() === "width",
-        "An ArgumentException with argument name 'width' is thrown")
+        "An ArgumentException with argument name 'width' is thrown"
+        )
     );
 
 test("HeightNonInteger", () =>
     throws(() => new ArrayScene(7, 1.5),
         (e: ArgumentException) => e.getArgumentName() === "height",
-        "An ArgumentException with argument name 'height' is thrown")
+        "An ArgumentException with argument name 'height' is thrown"
+        )
     );
 
 test("HeightNaN", () =>
     throws(() => new ArrayScene(7, Number.NaN),
         (e: ArgumentException) => e.getArgumentName() === "height",
-        "An ArgumentException with argument name 'height' is thrown")
+        "An ArgumentException with argument name 'height' is thrown"
+        )
     );
 
 test("HeightNegativeInfinity", () =>
     throws(() => new ArrayScene(7, Number.NEGATIVE_INFINITY),
         (e: ArgumentException) => e.getArgumentName() === "height",
-        "An ArgumentException with argument name 'height' is thrown")
+        "An ArgumentException with argument name 'height' is thrown"
+        )
     );
 
 test("HeightPositiveInfinity", () =>
     throws(() => new ArrayScene(7, Number.POSITIVE_INFINITY),
         (e: ArgumentException) => e.getArgumentName() === "height",
-        "An ArgumentException with argument name 'height' is thrown")
+        "An ArgumentException with argument name 'height' is thrown"
+        )
     );
 
 test("HeightNonPositive", () =>
     throws(() => new ArrayScene(7, 0),
         (e: ArgumentException) => e.getArgumentName() === "height",
-        "An ArgumentException with argument name 'height' is thrown")
+        "An ArgumentException with argument name 'height' is thrown"
+        )
     );
 
 var a: IScene;
@@ -98,62 +108,66 @@ test("FalseGet", () => {
 test("xNonInteger", () => {
     throws(() => a.setPoint(1.5, 7, true),
         (e: ArgumentException) => e.getArgumentName() === "x",
-        "An ArgumentException with argument name 'x' is thrown");
+        "An ArgumentException with argument name 'x' is thrown"
+        );
 });
 
 test("xNaN", () => {
     throws(() => a.setPoint(Number.NaN, 7, true),
         (e: ArgumentException) => e.getArgumentName() === "x",
-        "An ArgumentException with argument name 'x' is thrown");
+        "An ArgumentException with argument name 'x' is thrown"
+        );
 });
 
 test("xNegativeInfinity", () => {
     throws(() => a.setPoint(Number.NEGATIVE_INFINITY, 7, true),
         (e: ArgumentException) => e.getArgumentName() === "x",
-        "An ArgumentException with argument name 'x' is thrown");
+        "An ArgumentException with argument name 'x' is thrown"
+        );
 });
 
 test("xPositiveInfinity", () => {
     throws(() => a.setPoint(Number.POSITIVE_INFINITY, 7, true),
         (e: ArgumentException) => e.getArgumentName() === "x",
-        "An ArgumentException with argument name 'x' is thrown");
+        "An ArgumentException with argument name 'x' is thrown"
+        );
 });
 
 test("yNonInteger", () => {
     throws(() => a.setPoint(7, 1.5, true),
         (e: ArgumentException) => e.getArgumentName() === "y",
-        "An ArgumentException with argument name 'y' is thrown");
+        "An ArgumentException with argument name 'y' is thrown"
+        );
 });
 
 test("yNaN", () => {
     throws(() => a.setPoint(7, Number.NaN, true),
         (e: ArgumentException) => e.getArgumentName() === "y",
-        "An ArgumentException with argument name 'y' is thrown");
+        "An ArgumentException with argument name 'y' is thrown"
+        );
 });
 
 test("yNegativeInfinity", () => {
     throws(() => a.setPoint(7, Number.NEGATIVE_INFINITY, true),
         (e: ArgumentException) => e.getArgumentName() === "y",
-        "An ArgumentEyception with argument name 'y' is thrown");
+        "An ArgumentEyception with argument name 'y' is thrown"
+        );
 });
 
 test("yPositiveInfinity", () => {
     throws(() => a.setPoint(7, Number.POSITIVE_INFINITY, true),
         (e: ArgumentException) => e.getArgumentName() === "y",
-        "An ArgumentException with argument name 'y' is thrown");
+        "An ArgumentException with argument name 'y' is thrown"
+        );
 });
 
-QUnit.module("ArrayScene performance", {
-    setup: () => { a = new ArrayScene(2000, 2000); }
-});
+QUnit.module("ArrayScene performance");
 
 test("GetPerformanceWithInt", () => {
     var range = new Int(2000).getRange();
     var startTime = new Date().getTime();
 
-    range.For(x =>
-        range.For(y =>
-            a.getPoint(x, y)));
+    range.For(x => range.For(y => a.getPoint(x, y)));
 
     var duration = new Date().getTime() - startTime;
     strictEqual(false, false, "Duration: " + duration);
