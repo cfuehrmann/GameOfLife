c473690b176103d3b142cde049f2980009d4b3d1
cfuehrmann <cfuhrmann@gmail.com>
2014-05-30 18:35:56 +0200

    Added forgotten test-configuration file for requirejs

---
+++ GameOfLife/testmain.js
@@ -0,0 +1,23 @@
+﻿"use strict";
+require.config({
+    paths: {
+        'QUnit': 'Scripts/qunit'
+    },
+    shim: {
+        'QUnit': {
+            exports: 'QUnit',
+            init: function () {
+                QUnit.config.autoload = false;
+                QUnit.config.autostart = false;
+            }
+        }
+    }
+});
+
+require(['QUnit', 'ArraySceneTests'], function (QUnit, arraySceneTests) {
+    arraySceneTests.run();
+
+    QUnit.load();
+    QUnit.start();
+});
+
