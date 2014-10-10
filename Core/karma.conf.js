44e3430cdab64fd08d1adab1294c457c8a3a5896
cfuehrmann <cfuhrmann@gmail.com>
2014-10-10 19:33:20 +0200

    Added forgotten change to Core karma config; added missing jshintignore lines to GameOfLife

--- Core/karma.conf.js
+++ Core/karma.conf.js
@@ -29,6 +29,19 @@ module.exports = function (config) { // jshint ignore:line
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
         browsers: ['PhantomJS'],
 
+        phantomjsLauncher: {
+            // Here we could configure the path to the PhantomJS executable for each 
+            // platform. But since this file is a source, and the path is user-specific,
+            // the path should be set as an environment variable, not here.
+            // However, we need this "phantomjsLauncher" element, because 
+            // karma-phantomjs-launcher-nonet fails without it.
+            // cmd: {
+            //  linux: path.join(__dirname, 'target/phantomjs/linux64/phantomjs'),
+            //  darwin: path.join(__dirname, 'target/phantomjs/darwin/phantomjs'),
+            //  win32: path.join(__dirname, 'target/phantomjs/win/phantomjs.exe')
+            // }
+        },
+
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
         singleRun: true
