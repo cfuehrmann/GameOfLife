2870ec411819fb3fc609b7d069173d7b3e95ff7a
cfuehrmann <cfuhrmann@gmail.com>
2014-10-10 19:49:17 +0200

    Fixed jshintignore for Core; extended tests for Int

--- Core/Integers_test.ts
+++ Core/Integers_test.ts
@@ -5,10 +5,22 @@ import Int = Integers.Int;
 import Exceptions = require("Exceptions");
 import ArgumentException = Exceptions.ArgumentException;
 
-QUnit.module("Foo");
+QUnit.module("Int");
 
-test("FooTest", () => {
+test("ConstructorWithFiniteNonInteger", () => {
     throws(() => new Int(0.5),
         (e: ArgumentException) => e.getArgumentName() === "value",
-        "An ArgumentException with argument name 'value' is thrown");
+        "No ArgumentException with argument name 'value' is thrown");
+});
+
+test("ConstructorWithInfinity", () => {
+    throws(() => new Int(Infinity),
+        (e: ArgumentException) => e.getArgumentName() === "value",
+        "No ArgumentException with argument name 'value' is thrown");
+});
+
+test("ConstructorWithMinusInfinity", () => {
+    throws(() => new Int(-Infinity),
+        (e: ArgumentException) => e.getArgumentName() === "value",
+        "No ArgumentException with argument name 'value' is thrown");
 });
