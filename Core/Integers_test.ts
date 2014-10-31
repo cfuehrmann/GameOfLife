1ca859a5218d366aa66b4d305924866d7ef7bed9
cfuehrmann <cfuhrmann@gmail.com>
2014-10-31 18:14:14 +0100

    Added tests for EagerRange.For

--- Core/Integers_test.ts
+++ Core/Integers_test.ts
@@ -1,9 +1,10 @@
 ﻿/// <reference path="Imports/QUnit/qunit.d.ts" />
 
-
 import Integers = require("Integers");
 import Int = Integers.Int;
 import EagerRange = Integers.EagerRange;
+import Sequences = require("Sequences");
+import Seq = Sequences.Seq;
 import Exceptions = require("Exceptions");
 import ArgumentException = Exceptions.ArgumentException;
 
@@ -46,8 +47,56 @@ test("getValue", () => {
 
 QUnit.module("EagerRange");
 
-test("Constructor", () => {
+test("ConstructorWithNegativeArgument", () => {
     throws(() => new EagerRange(new Int(-1)),
         (e: ArgumentException) => e.getArgumentName() === "value",
         "No ArgumentException with argument name 'value' is thrown");
 });
+
+test("For", () => {
+    var calls: Array<Number> = new Array<Number>();
+    function body(i: Int) {
+        calls.push(i.getValue());
+    }
+    var range: Seq<Int> = new EagerRange(new Int(42));
+
+    range.For(body);
+
+    strictEqual(calls.length, 42);
+    for (var i = 0; i < 42; i++) {
+        strictEqual(calls[i], i);
+    }
+});
+
+test("ForWhenBodyThrows", () => {
+    var calls: Array<Number> = new Array<Number>();
+    function body(i: Int) {
+        if (i.getValue() === 3) {
+            throw "foo";
+        } else {
+            calls.push(i.getValue());
+        }
+    }
+    var range: Seq<Int> = new EagerRange(new Int(42));
+
+    throws(() => range.For(body), "foo", "The exception from the body is not propagated!");
+
+    strictEqual(calls.length, 3);
+    for (var i = 0; i < 3; i++) {
+        strictEqual(calls[i], i);
+    }
+});
+
+test("ForOnZeroRange", () => {
+    var bodyHasBeenCalled: boolean = false;
+    function body(i: Int) {
+        bodyHasBeenCalled = true;
+    }
+    var range: Seq<Int> = new EagerRange(new Int(0));
+
+    range.For(body);
+
+    strictEqual(bodyHasBeenCalled, false);
+});
+
+
