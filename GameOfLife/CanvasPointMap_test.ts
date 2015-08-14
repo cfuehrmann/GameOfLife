eba7d4703b5ae5096d58ade79730a655a6c8076b
cfuehrmann <cfuhrmann@gmail.com>
2015-08-14 19:27:24 +0200

    Added token test for CanvasPointMap

---
+++ GameOfLife/CanvasPointMap_test.ts
@@ -0,0 +1,18 @@
+﻿import CanvasPointMap = require("CanvasPointMap");
+import TypeChecking = require("Imports/Core/TypeChecking");
+let method: string;
+let name = (testCase: string) => "CanvasPointMap, " + method + ": " + testCase;
+
+method = "create";
+
+test(name("height is integer"),
+    TypeChecking.checkIntAssert("height", height => CanvasPointMap.create(height, 1, 1))
+);
+
+test(name("width is integer"),
+    TypeChecking.checkIntAssert("width", width => CanvasPointMap.create(1, width, 1))
+);
+
+test(name("pointSize is integer"),
+    TypeChecking.checkIntAssert("pointSize", pointSize => CanvasPointMap.create(1, 1, pointSize))
+);
